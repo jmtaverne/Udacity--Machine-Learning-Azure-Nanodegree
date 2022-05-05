3d7ea63565a4d634b24ba4de97989ae09b209fbf
Jean-Michel Taverne <jm.taverne@web.de>
2022-05-05 17:49:47 +0200

    Add files via upload

---
+++ Project 2 - Operationalization Machine Learning/Consume Endpoint.py
@@ -0,0 +1,93 @@
+
+import urllib.request
+import json
+import os
+import ssl
+
+# URL for the web service, should be similar to:
+# 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
+url = 'http://75159c5b-2fac-425c-8872-4677c8f682dc.eastus.azurecontainer.io/score'
+# If the service is authenticated, set the key or token
+key = '2lxK7VhWR1ih0fBWMeMUPGQ4wm4RYGRR'
+
+
+def allowSelfSignedHttps(allowed):
+    # bypass the server certificate verification on client side
+    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+
+# Two sets of data to score, so we get two results back
+data = {
+    "Inputs": {
+        "data":
+        [
+          {
+            "age": 17,
+            "campaign": 1,
+            "cons.conf.idx": -46.2,
+            "cons.price.idx": 92.893,
+            "contact": "cellular",
+            "day_of_week": "mon",
+            "default": "no",
+            "duration": 971,
+            "education": "university.degree",
+            "emp.var.rate": -1.8,
+            "euribor3m": 1.299,
+            "housing": "yes",
+            "job": "blue-collar",
+            "loan": "yes",
+            "marital": "married",
+            "month": "may",
+            "nr.employed": 5099.1,
+            "pdays": 999,
+            "poutcome": "failure",
+            "previous": 1
+          },
+          {
+            "age": 87,
+            "campaign": 1,
+            "cons.conf.idx": -46.2,
+            "cons.price.idx": 92.893,
+            "contact": "cellular",
+            "day_of_week": "mon",
+            "default": "no",
+            "duration": 471,
+            "education": "university.degree",
+            "emp.var.rate": -1.8,
+            "euribor3m": 1.299,
+            "housing": "yes",
+            "job": "blue-collar",
+            "loan": "yes",
+            "marital": "married",
+            "month": "may",
+            "nr.employed": 5099.1,
+            "pdays": 999,
+            "poutcome": "failure",
+            "previous": 1
+          },
+        ]
+    },
+    "GlobalParameters": {
+        "method": "predict"
+    }
+}
+
+body = str.encode(json.dumps(data))
+
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ key)}
+
+req = urllib.request.Request(url, body, headers)
+
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result)
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+    #Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+    print(error.read().decode("utf8", 'ignore'))
